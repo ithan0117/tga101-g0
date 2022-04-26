6020d09d2a2f3dd872a9939613cf9a1537cc56d3
William Lee <ithan0117@gmail.com>
2022-04-26 16:19:00 +0800

    comment test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,10 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 
+ * @author william
+ *
+ */
 @WebServlet("/hello")
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	/**
+	 * 未來團隊可能會要求打方法的註解唷！
+	 * @param request 請求物件
+	 * @param response 回應物件
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
